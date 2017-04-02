7f5a13328f3e4be4239f9f7a857a250a1ad696a0
Iván Portillo Leal <ivanportillo@me.com>
2017-04-02 21:36:25 +0200

    Added findByEmail to user repository

--- backend/repository/userRepository.js
+++ backend/repository/userRepository.js
@@ -1,14 +1,21 @@
 'use strict';
 
+const NotFoundError = require('spur-errors').NotFoundError;
+
 module.exports = UserEntity => {
   const findOneById = (id, cb) => {
     const query = { id };
     UserEntity.one(query, cb);
   };
 
+  const findByEmail = (email, cb) => {
+    const query = { email };
+    UserEntity.findOne(query, cb);
+  };
+
   const create = (user, cb) => {
     UserEntity.create(user, cb);
   };
 
-  return { findOneById, create };
+  return { findOneById, create, findByEmail };
 };
