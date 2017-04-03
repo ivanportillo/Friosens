b3dbeaed9a753d4141bb377303e8bafc98da9747
Iván Portillo Leal <ivanportillo@me.com>
2017-04-03 21:29:14 +0200

    Created background to user step definitions

---
+++ backend/test/features/step_definitions/user/background.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { defineSupportCode } = require('cucumber');
+const async = require('async');
+
+const repositories = require('../../../../repository');
+const userRepository = repositories.User;
+const encrypt = require('../../../../infrastructure/passEncrypt');
+
+defineSupportCode(({ Given }) => {
+    Given('there is the following users:', function (table, callback) {
+        const users = table.hashes();
+        async.each(users, (user, done) => {
+            encrypt.encrypt(user.password, (err, hash, salt) => {
+                if(err) done(err);
+                else {
+                    user.salt = salt;
+                    user.password = hash;
+                    user.enabled = (user.enabled === 'true');
+                    userRepository.create(user, err => {
+                        if (err) done(err);
+                        else done();
+                    });
+                }
+            });
+        }, callback);
+    });
+});
