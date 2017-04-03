f6420d12b48b344258cac85320cba93981c9751b
Iván Portillo Leal <ivanportillo@me.com>
2017-04-03 21:26:46 +0200

    Added Login Path

--- backend/router/index.js
+++ backend/router/index.js
@@ -2,11 +2,16 @@
 
 const PATHS = require('./paths');
 const facilitiesController = require('./controllers/facilities');
+const usersController = require('./controllers/users');
 
 module.exports = router => {
+  // FACILITY
   router.post(PATHS.FACILITIES_PATH, facilitiesController.createFacility);
   router.delete(PATHS.FACILITY_PATH, facilitiesController.removeFacility);
   router.put(PATHS.FACILITY_PATH, facilitiesController.updateFacility);
+
+  // AUTH
+  router.post(PATHS.LOGIN_PATH, usersController.login);
 
   return router;
 };
