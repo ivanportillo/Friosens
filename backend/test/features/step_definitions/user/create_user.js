fdf8266114d0a6e96118691f5ab568c040c541d2
Iván Portillo Leal <ivanportillo@me.com>
2017-04-05 22:00:18 +0200

    Created step definitions to create user feature

--- backend/test/features/step_definitions/user/create_user.js
+++ backend/test/features/step_definitions/user/create_user.js
@@ -1,3 +1,60 @@
-/**
- * Created by ivanportilloleal on 05/04/2017.
- */
+'use strict';
+
+const should = require('should');
+const { defineSupportCode } = require('cucumber');
+
+const PATHS = require('../../../../router/paths');
+const request = require('../../support/request');
+
+const tokenUt = require('../../../utils/factories/User_factory')();
+
+defineSupportCode(({ Given, When, Then }) => {
+    let token;
+    let responseCreate;
+    Given('[create-user] I\'m logged in as an administrator', function (done) {
+        const user = {
+            first_name: 'Nombre',
+            last_name: 'Apellidos',
+            enabled: true,
+            admin: true,
+            email: 'email@dominio.es',
+            password: 'password'
+        };
+        tokenUt.createLogged(user, (err, tokenResult) => {
+           if(err) done(err);
+           else {
+               token = tokenResult;
+               done();
+           }
+        });
+    });
+
+    When('I create a new user with the following data:', function (table, done) {
+        const user = table.hashes()[0];
+        request.post(PATHS.REGISTER_PATH, user, token, (error, response, statusCode) => {
+            responseCreate = { error, response, statusCode };
+            done();
+        });
+    });
+
+    Then('I should be able to login with {stringInDoubleQuotes} as username and {stringInDoubleQuotes} as password', function (stringInDoubleQuotes, stringInDoubleQuotes2, done) {
+        const email = stringInDoubleQuotes;
+        const password = stringInDoubleQuotes2;
+        const payload = { email, password };
+        request.post(PATHS.LOGIN_PATH, payload, null, (error, response, statusCode) => {
+           should.not.exists(error);
+           statusCode.should.be.eql(200);
+           response.should.have.key('token');
+           done();
+        });
+    });
+
+    Then('[create-user] I should receive a validation error with code {int} and message {stringInDoubleQuotes}', function (int, stringInDoubleQuotes, done) {
+        const errorCode = int;
+        const message = stringInDoubleQuotes;
+        responseCreate.statusCode.should.be.eql(errorCode);
+        should.exist(responseCreate.response.error);
+        responseCreate.response.error.should.be.eql(message);
+        done();
+    });
+});
