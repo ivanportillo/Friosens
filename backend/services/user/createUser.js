4199713f53d5c5480f1e8c875cf46665349af6a0
Iván Portillo Leal <ivanportillo@me.com>
2017-04-05 22:01:11 +0200

    Created service to create a new user

---
+++ backend/services/user/createUser.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const waterfall = require('async').waterfall;
+const ValidationError = require('spur-errors').ValidationError;
+
+module.exports = (UserRepository, encrypt) => {
+    const _validateFields = (user, cb) => {
+        const {
+            first_name,
+            last_name,
+            company,
+            address,
+            telephone,
+            email,
+            password,
+            admin
+        } = user;
+        const error = msg => cb(ValidationError.create(msg));
+        // http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!email) error('Email is required');
+        else if(!emailRegex.test(email)) error('Invalid email');
+        else if(!first_name) error('First name is required');
+        else if(!last_name) error('Last name is required');
+        else if(!password) error('Password is required');
+        else cb();
+        // TO-DO: More validation
+    };
+
+    const _encryptPassword = (user, cb) => {
+        encrypt.encrypt(user.password, (err, hash, salt) => {
+           if (err) cb(err);
+           else {
+               user.password = hash;
+               user.salt = salt;
+               cb(null, user);
+           }
+        });
+    };
+
+    const _saveUser = (user, cb) => {
+        user.enabled = 1;
+        UserRepository.create(user, cb);
+    };
+
+    return (user, callback) => waterfall([
+       next => _validateFields(user, next),
+       next => _encryptPassword(user, next),
+       (finalUser, next) => _saveUser(finalUser, next)
+    ], callback);
+};
