21bba8407a42bb7fda4005543f9c141d551a1f9a
Iván Portillo Leal <ivanportillo@me.com>
2017-04-06 17:44:14 +0200

    Refactored create facility step definitions

--- backend/test/features/step_definitions/facility/create_facility.js
+++ backend/test/features/step_definitions/facility/create_facility.js
@@ -4,34 +4,42 @@ const should = require('should');
 const { defineSupportCode } = require('cucumber');
 
 const repositories = require('../../../../repository');
-const userRepository = repositories.User;
 const facilityRepository = repositories.Facility;
-const unitRepository = repositories.Unit;
 
-const facilityActions = require('../../../../action/facility');
-const unitActions = require('../../../../action/unit');
-
-const createFacility = facilityActions.createFacility;
-const removeFacility = facilityActions.removeFacility;
-const createUnit = unitActions.createUnit;
+const tokenUt = require('../../../utils/factories/User_factory')();
 
 const PATHS = require('../../../../router/paths');
 const request = require('../../support/request');
 
-defineSupportCode(({ When, Then }) => {
+defineSupportCode(({ Given, When, Then }) => {
+  let token;
+  let responseCreate;
+  Given('[create-facility] I\'m logged in as an administrator', function (done) {
+      const user = {
+          first_name: 'Nombre',
+          last_name: 'Apellidos',
+          enabled: true,
+          admin: true,
+          email: 'email@dominio.es',
+          password: 'password'
+      };
+      tokenUt.createLogged(user, (err, tokenResult) => {
+          if(err) done(err);
+          else {
+              token = tokenResult;
+              done();
+          }
+      });
+  });
   When('I create to the user with ID {int} the following facility:', (int, table, done) => {
     const payload = {
       name: table.hashes()[0].name,
       location: table.hashes()[0].location,
       userId: int
     };
-
-    request.post(PATHS.FACILITIES_PATH, payload, null, (error, response, statusCode) => {
-      should.not.exists(error);
-      statusCode.should.be.eql(200);
-      response.item.name.should.be.eql(payload.name);
-      response.item.location.should.be.eql(payload.location);
-      response.item.user_id.should.be.eql(payload.userId);
+    request.post(PATHS.FACILITIES_PATH, payload, token, (error, response, statusCode) => {
+      should.not.exist(error);
+      responseCreate = { response, statusCode };
       done();
     });
   });
@@ -39,13 +47,21 @@ defineSupportCode(({ When, Then }) => {
   Then('User with ID {int} should have one facility with name {stringInDoubleQuotes}', (int, stringInDoubleQuotes, done) => {
     const userId = int;
     const facilityName = stringInDoubleQuotes;
-    facilityRepository.findByUserId(userId, (err, facility) => {
-      if(err) done(err);
-      else {
-        facility.user_id.should.be.eql(userId);
-        facility.name.should.be.eql(facilityName);
-        done();
-      }
-    });
+      facilityRepository.findByUserId(userId, (err, facility) => {
+          if(err) done(err);
+          else {
+              facility.user_id.should.be.eql(userId);
+              facility.name.should.be.eql(facilityName);
+              done();
+          }
+      });
+  });
+
+  Then('the response is a validation error with code {int} and message {stringInDoubleQuotes}', function (int, stringInDoubleQuotes, done) {
+    const errorCode = int;
+    const error = stringInDoubleQuotes;
+    responseCreate.statusCode.should.be.eql(errorCode);
+    responseCreate.response.error.should.be.eql(error);
+    done();
   });
 });
