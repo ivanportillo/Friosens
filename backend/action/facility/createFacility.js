32487dd8b272fac9a72b6a59288e75d157109c91
Iván Portillo Leal <ivanportillo@me.com>
2017-04-06 17:46:24 +0200

    Added validation create facility action

--- backend/action/facility/createFacility.js
+++ backend/action/facility/createFacility.js
@@ -1,11 +1,20 @@
 'use strict';
 
 const waterfall = require('async').waterfall;
+const ValidationError = require('spur-errors').ValidationError;
 
 module.exports = facilityRepository => {
+  const _validate = (facility, cb) => {
+    if(!facility.name) cb(ValidationError.create('Name is required'));
+    else cb();
+  };
+
   const _createFacility = (facility, userId, cb) => {
     facilityRepository.create(facility, userId, cb);
   };
 
-  return (facility, userId, callback) => _createFacility(facility, userId, callback);
+  return (facility, userId, callback) => waterfall([
+    next => _validate(facility, next),
+    next => _createFacility(facility, userId, next)
+  ], callback);
 };
