0e0413ac02ed805d0b6e7f849e16ecbf2366be97
Iván Portillo Leal <ivanportillo@me.com>
2017-04-06 22:09:34 +0200

    Added admin and id properties to token payload

--- backend/services/user/tokenManager.js
+++ backend/services/user/tokenManager.js
@@ -5,13 +5,14 @@ const UnauthorizedError = require('spur-errors').UnauthorizedError;
 module.exports = tokenService => {
   const create = (user, ttl, cb) => {
     const payload = {
+      id: user.id,
       first_name: user.first_name,
       last_name: user.last_name,
       company: user.company,
       telephone: user.telephone,
-      email: user.email
+      email: user.email,
+      admin: user.admin
     };
-
     tokenService.create(payload, ttl, (err, token) => {
       if (err) cb(err);
       else cb(null, token);
