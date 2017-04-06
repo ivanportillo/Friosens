bb5fa2a3e09058805578ff2f9b3caf99d725876a
Iván Portillo Leal <ivanportillo@me.com>
2017-04-06 23:23:16 +0200

    Added showFacilities controller and removed updateFacility

--- backend/router/controllers/facilities.js
+++ backend/router/controllers/facilities.js
@@ -3,6 +3,7 @@
 const facilitiesActions = require('../../action/facility');
 const createFacility = facilitiesActions.createFacility;
 const removeFacility = facilitiesActions.removeFacility;
+const showFacilities = facilitiesActions.showFacilities;
 
 const responseBuilder = require('../../utils/responseBuilder');
 
@@ -12,13 +13,12 @@ module.exports = {
       name: req.body.name,
       location: req.body.location
     };
-
     createFacility(facility, req.body.userId, responseBuilder.createResponse(req, res));
   },
   removeFacility: (req, res) => {
     removeFacility(req.params.id, responseBuilder.createResponse(req, res));
   },
-  updateFacility: (req, res) => {
-
+  showFacilities: (req, res) => {
+    showFacilities(req.payload.id, responseBuilder.createResponse(req, res));
   }
 };
