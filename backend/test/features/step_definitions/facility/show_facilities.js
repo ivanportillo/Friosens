d0ba0fed46eb620e7da58d753a46969ccac8c1e6
Iván Portillo Leal <ivanportillo@me.com>
2017-04-06 23:21:30 +0200

    Created show facilities step definitions

---
+++ backend/test/features/step_definitions/facility/show_facilities.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const should = require('should');
+const { defineSupportCode } = require('cucumber');
+
+const tokenUt = require('../../../utils/factories/User_factory')();
+
+const PATHS = require('../../../../router/paths');
+const request = require('../../support/request');
+
+defineSupportCode(({ Given, When, Then }) => {
+  let token;
+  let showResponse;
+  Given(/^I'm logged as user ID (\d+)$/, (id, done) => {
+    const user = {
+      id,
+      first_name: 'Nombre',
+      last_name: 'Apellidos',
+      enabled: true,
+      admin: false,
+      email: 'email@dominio.es',
+      password: 'password'
+    };
+    tokenUt.createLogged(user, (err, tokenResult) => {
+      if(err) done(err);
+      else {
+        token = tokenResult;
+        done();
+      }
+    });
+  });
+
+  When(/^I get my facilities$/, done => {
+    request.get(PATHS.FACILITIES_PATH, null, token, (error, response, statusCode) => {
+      should.not.exist(error);
+      showResponse = { response, statusCode };
+      done();
+    });
+  });
+
+  Then(/^I should receive (\d+) facilities and (\d+) as status code$/, (nFacilities, statusCode, done) => {
+    const facilities = showResponse.response.data;
+    facilities.length.should.be.eql(nFacilities);
+    showResponse.statusCode.should.be.eql(statusCode);
+    done();
+  });
+});
