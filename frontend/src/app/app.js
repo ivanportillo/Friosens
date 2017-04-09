15fe007c3f28c8aaabebbfae5cd0d7cc6ecf4c9e
Iván Portillo Leal <ivanportillo@me.com>
2017-04-09 16:32:14 +0200

    Added redux and redux devtools

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import 'normalize.css';
 
-import routes from 'app/routes';
+import routes from 'routes';
+import reducers from 'reducers';
+import sagas from 'sagas';
 
-render(routes(), document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducers,
+  composeWithDevTools(
+    applyMiddleware(
+      sagaMiddleware,
+      routerMiddleware(browserHistory),
+    ),
+  ),
+);
+
+sagaMiddleware.run(sagas);
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      {routes()}
+    </Router>
+  </Provider>, document.getElementById('root'));
