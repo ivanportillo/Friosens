8b4ba40d89464cfe46c51e7a8059cade526ea74b
Iván Portillo Leal <ivanportillo@me.com>
2017-04-09 23:10:31 +0200

    Created PATHS and modified routes

--- frontend/src/app/routes/index.js
+++ frontend/src/app/routes/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import { Route } from 'react-router';
-import Login from 'components/Login';
 
-const routes = () =>
-  <Route path="/" component={Login} />;
+import Login from 'containers/Login';
+import Layout from 'components/Layout';
 
-export default routes;
+import { LOGIN_PATH, ROOT_PATH } from './paths';
+
+const createRoutes = store => {
+  const routes = (
+    <Route>
+      <Route onEnter={requiredNotAuthenticated} path="/login" component={Login} />
+      <Route onEnter={requiredAuthenticated} path="/" component={Layout} />
+    </Route>);
+
+  function requiredAuthenticated(nextState, replace, fn) {
+    const state = store.getState();
+    if (!state.auth.user && !state.auth.isBooting){
+      replace(LOGIN_PATH);
+    }
+    fn();
+  }
+
+  function requiredNotAuthenticated(nextState, replace, fn) {
+    const state = store.getState();
+    if (state.auth.user && !state.auth.isBooting){
+      replace(ROOT_PATH);
+    }
+    fn();
+  }
+
+  return routes;
+};
+
+
+export default createRoutes;
