dc92220f8520a7aa70ee3e410d8d0f601a273993
Iván Portillo Leal <ivanportillo@me.com>
2017-04-09 23:09:56 +0200

    Created api functions

---
+++ frontend/src/app/api/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import apiPaths from '../../../../backend/router/paths';
+
+import { getToken } from 'utils/token';
+
+const API_BASE =
+  (process.env.NODE_ENV === 'production') ? `https://${window.location.hostname}` :
+                                            `http://${window.location.hostname}:3000`;
+
+export const login = (email, password) => axios.post(apiPaths.LOGIN_PATH,{
+  email,
+  password,
+}, {
+  baseURL: API_BASE,
+});
+
+export const fetchAccount = () => axios.get(apiPaths.ACCOUNT_PATH, {
+  baseURL: API_BASE,
+  headers: { Authorization: getToken() },
+});
