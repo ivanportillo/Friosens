b081fb0637ff17f5246624d91296fcf36ba22571
Iván Portillo Leal <ivanportillo@me.com>
2017-04-10 23:49:01 +0200

    Added uiReducer to root reducer

--- frontend/src/app/reducers/index.js
+++ frontend/src/app/reducers/index.js
@@ -3,11 +3,13 @@ import { routerReducer } from 'react-router-redux';
 import { combineReducers } from 'redux';
 
 import authReducer from './auth';
+import uiReducer from './ui';
 
 export default combineReducers({
   routing: routerReducer,
   form: formReducer,
   auth: authReducer,
+  ui: uiReducer,
 });
 
 
