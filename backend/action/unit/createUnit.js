326ad66a51a198907b75b01359a9445c08d02ea1
Iván Portillo Leal <ivanportillo@me.com>
2017-04-11 22:56:22 +0200

    Add validation to create unit Action

--- backend/action/unit/createUnit.js
+++ backend/action/unit/createUnit.js
@@ -2,10 +2,21 @@
 
 const waterfall = require('async').waterfall;
 
+const ValidationError = require('spur-errors').ValidationError;
+
 module.exports = unitRepository => {
+  const _validate = (unit, cb) => {
+    if(!unit.name) cb(ValidationError.create('Name is required'));
+    else if(!unit.refrigerant) cb(ValidationError.create('Refrigerant is required'));
+    else cb();
+  };
+
   const _createUnit = (unit, facilityId, cb) => {
     unitRepository.create(unit, facilityId, cb);
   };
 
-  return (unit, facilityId, cb) => _createUnit(unit, facilityId, cb);
+  return (facilityId, unit, callback) => waterfall([
+    next => _validate(unit, next),
+    next => _createUnit(unit, facilityId, next),
+  ], callback);
 };
