22450953a9612f7a23f5a030c10f9539cb804654
Iván Portillo Leal <ivanportillo@me.com>
2017-04-11 22:08:19 +0200

    Refactored start db task

--- backend/tasks/start.js
+++ backend/tasks/start.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = db => () => {
-  
-};
+const database = require('../utils/database');
+
+const models = require('../models');
+
+const db = database.getDb();
+
+db.drop(err => {
+  if(err) return console.log(err);
+  db.sync(err => {
+    if (err) console.log(err);
+    else console.log("Done!");
+    db.close();
+  });
+});
