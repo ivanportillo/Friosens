cfa68f1323a992dae487d0acccf28cbbddc67b7b
Iván Portillo Leal <ivanportillo@me.com>
2017-04-11 22:55:50 +0200

    Create controller to Create Unit

--- backend/router/controllers/facilities.js
+++ backend/router/controllers/facilities.js
@@ -7,6 +7,7 @@ const showFacilities = facilitiesActions.showFacilities;
 
 const unitsActions = require('../../action/unit');
 const showUnits = unitsActions.showUnits;
+const createUnit = unitsActions.createUnit;
 
 const responseBuilder = require('../../utils/responseBuilder');
 
@@ -26,5 +27,14 @@ module.exports = {
   },
   getUnits: (req, res) => {
     showUnits(req.params.id, responseBuilder.createResponse(req, res));
+  },
+  createUnit: (req, res) => {
+    const unit = {
+      name: req.body.name,
+      location: req.body.location,
+      refrigerant: req.body.refrigerant,
+      mark: req.body.mark
+    };
+    createUnit(req.params.id, unit, responseBuilder.createResponse(req, res));
   }
 };
--- backend/router/index.js
+++ backend/router/index.js
@@ -14,7 +14,10 @@ module.exports = router => {
   router.post(PATHS.FACILITIES_PATH, allowAccessAdmin, facilitiesController.createFacility);
   router.get(PATHS.FACILITIES_PATH, allowAccessUser, facilitiesController.showFacilities);
   router.delete(PATHS.FACILITY_PATH, allowAccessAdmin, facilitiesController.removeFacility);
+
+  // UNITS (INSIDE FACILITY)
   router.get(PATHS.FACILITY_UNITS_PATH, allowAccessUser, facilitiesController.getUnits);
+  router.post(PATHS.FACILITY_UNITS_PATH, allowAccessAdmin, facilitiesController.createUnit);
 
   // AUTH
   router.post(PATHS.LOGIN_PATH, usersController.login);
