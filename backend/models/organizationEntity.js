491c404b6b1509640a616f62195c2d0611fcc29a
Iván Portillo Leal <ivanportillo@me.com>
2017-04-13 19:04:35 +0200

    Modify organization entity to change type to required

--- backend/models/organizationEntity.js
+++ backend/models/organizationEntity.js
@@ -3,6 +3,6 @@
 module.exports = (orm, db) => {
   const Organization = db.define('organization', {
     name: { type: 'text', required: true },
-    type: { type: 'enum', values: ["particular", "company", "freelance"] }
+    type: { type: 'enum', values: ["particular", "company", "freelance"], required: true }
   });
 };
