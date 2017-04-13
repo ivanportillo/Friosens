11e36a435be235e15f3e6a1436c17b935cdd25d3
Iván Portillo Leal <ivanportillo@me.com>
2017-04-13 19:04:02 +0200

    Create path to organization

--- backend/router/paths.js
+++ backend/router/paths.js
@@ -7,6 +7,10 @@ const prefix = path => PREFIX + path;
 exports.LOGIN_PATH = prefix('/login');
 exports.LOGOUT_PATH = prefix('/logout');
 
+// ORGANIZATION
+exports.ORGANIZATIONS_PATH = prefix('/organizations');
+exports.ORGANIZATION_PATH = prefix('/organizations/:organization');
+
 //ACCOUNT
 exports.REGISTER_PATH = prefix('/register');
 exports.ACCOUNT_PATH = prefix('/account');
