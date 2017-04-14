a7d682ae8a3bc5b3b475009b83ff8b9c2b9e2bfb
Iván Portillo Leal <ivanportillo@me.com>
2017-04-14 23:22:06 +0200

    Create reading repository

--- backend/repository/readingRepository.js
+++ backend/repository/readingRepository.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = ReadingEntity => {
+  const create = (reading, cb) => {
+    ReadingEntity.create(reading, cb);
+  };
 
+  const findByUnitId = (unitId, cb) => {
+    const query = { unit_id: unitId };
+    ReadingEntity.find(query, cb);
+  };
+
+  return { create, findByUnitId };
 };
