d43b3515c42e7ef45d45e8ab954f2be552f9ae24
Iván Portillo Leal <ivanportillo@me.com>
2017-04-14 20:10:10 +0200

    Add token generation to create unit action

--- backend/action/unit/createUnit.js
+++ backend/action/unit/createUnit.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const waterfall = require('async').waterfall;
-
+const config = require('config');
 const ValidationError = require('spur-errors').ValidationError;
 
-module.exports = unitRepository => {
+module.exports = (unitRepository, unitServices) => {
   const _validate = (unit, cb) => {
     if(!unit.name) cb(ValidationError.create('Name is required'));
     else if(!unit.refrigerant) cb(ValidationError.create('Refrigerant is required'));
@@ -15,8 +15,20 @@ module.exports = unitRepository => {
     unitRepository.create(unit, facilityId, cb);
   };
 
+  const _generateToken = (unit, cb) => {
+    const payload = { id: unit.id };
+    unitServices.tokenManager.create(payload, config.app.token.ttl, (err, token) => {
+      if (err) cb(err);
+      else {
+        unit.token = token;
+        unit.save(cb);
+      }
+    });
+  };
+
   return (facilityId, unit, callback) => waterfall([
     next => _validate(unit, next),
     next => _createUnit(unit, facilityId, next),
+    (unit, next) => _generateToken(unit, next),
   ], callback);
 };
--- backend/action/unit/index.js
+++ backend/action/unit/index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const unitRepository = require('../../repository').Unit;
+const unitServices = require('../../services/unit');
 
 const createCreateUnit = require('./createUnit');
 const createShowUnits = require('./showUnits');
 const createRemoveUnit = require('./removeUnit');
 
 module.exports = {
-  createUnit: createCreateUnit(unitRepository),
+  createUnit: createCreateUnit(unitRepository, unitServices),
   showUnits: createShowUnits(unitRepository),
   removeUnit: createRemoveUnit(unitRepository)
 };
