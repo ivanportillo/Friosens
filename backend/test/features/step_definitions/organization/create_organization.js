ded9c23efb12c3fe9bedec7b9dfd9026c1b9309a
Iván Portillo Leal <ivanportillo@me.com>
2017-04-14 12:31:29 +0200

    Create create organization step definitions

---
+++ backend/test/features/step_definitions/organization/create_organization.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const should = require('should');
+const { defineSupportCode } = require('cucumber');
+
+const tokenUt = require('../../../utils/factories/User_factory')();
+
+const organizationRepository = require('../../../../repository').Organization;
+
+const PATHS = require('../../../../router/paths');
+const request = require('../../support/request');
+
+defineSupportCode(({ Given, When, Then }) => {
+  let token;
+  let createResponse;
+  Given(/^\[create\-organization\] I'm logged in as an administrator$/, done => {
+    const user = {
+      first_name: 'Nombre',
+      last_name: 'Apellidos',
+      enabled: true,
+      admin: true,
+      email: 'email@dominio.es',
+      password: 'password'
+    };
+    tokenUt.createLogged(user, (err, tokenResult) => {
+      if(err) done(err);
+      else {
+        token = tokenResult;
+        done();
+      }
+    });
+  });
+
+  When(/^I create the following organization:$/, (table, done) => {
+    const organization = table.hashes()[0];
+    request.post(PATHS.ORGANIZATIONS_PATH, organization, token, (error, response, statusCode) => {
+      should.not.exist(error);
+      createResponse = { response, statusCode };
+      done();
+    });
+  });
+
+  Then(/^I should receive a response with status code (\d+) and "([^"]*)" as message$/, (statusCode, message, done) => {
+    createResponse.statusCode.should.be.eql(statusCode);
+    createResponse.response.message.should.be.eql(message);
+    done();
+  });
+  
+  Then(/^should exist a organization with name "([^"]*)"$/, (orgName, done) => {
+    organizationRepository.findOneByName(orgName, (err, organization) => {
+      if (err) done(err);
+      else {
+        organization.name.should.be.eql(orgName);
+        organization.type.should.not.be.empty();
+        done();
+      }
+    });
+  });
+
+  Then(/^I should receive a error with status code (\d+) and "([^"]*)" as message$/, (statusCode, message, done) => {
+    createResponse.statusCode.should.be.eql(statusCode);
+    createResponse.response.error.should.be.eql(message);
+    done();
+  });
+});
