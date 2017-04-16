e10501c68e98af4dc7f75ab7be74c9bb4cc81d82
Iván Portillo Leal <ivanportillo@me.com>
2017-04-16 13:29:12 +0200

    Create rules loader

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,16 @@ const routes = require('./router');
 const app = express();
 const Router = express.Router;
 
+const loadRules = require('./rules');
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(routes(new Router()));
+
+loadRules();
 
 app.listen(config.app.express.port, () => {
   if (process.env.NODE_ENV !== 'test') console.log(`API listening at port ${config.app.express.port}`);
---
+++ backend/rules/index.js
@@ -0,0 +1,34 @@
+const async = require('async');
+const ruleEngine = require('../utils/ruleEngine')();
+
+const getSpecificationRules = require('./getSpecificationsRules');
+const getAdditionalRules = require('./getAdditionalRules');
+
+const SPECIFICATIONS_PATH = 'rules/refrigerantSpecifications';
+const ADDITIONAL_RULES_PATH = 'rules/additionalRules';
+
+module.exports = () => {
+  const _loadSpecificationRules = (cb) => {
+    getSpecificationRules(SPECIFICATIONS_PATH, (err, result) => {
+      async.each(result, (rule, next) => {
+        ruleEngine.addRule(rule);
+        next();
+      }, cb);
+    });
+  };
+
+  const _loadAdditionalRules = (cb) => {
+    getAdditionalRules(ADDITIONAL_RULES_PATH, (err, result) => {
+      if(result.length) {
+        async.each(result, (rule, next) => {
+          ruleEngine.addRule(rule);
+          next();
+        });
+      } else cb();
+    });
+  };
+  return async.waterfall([
+    next => _loadSpecificationRules(next),
+    next => _loadAdditionalRules(next),
+  ]);
+};
