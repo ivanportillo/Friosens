7548fec63dc24d20d31d0eca27856404e9d26825
Iván Portillo Leal <ivanportillo@me.com>
2017-04-16 17:54:32 +0200

    Create register alarm step definitions

---
+++ backend/test/features/step_definitions/alarm/register_alarm.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const should = require('should');
+const { defineSupportCode } = require('cucumber');
+const async = require('async');
+
+const repositories = require('../../../../repository');
+const unitRepository = repositories.Unit;
+const alarmRepository = repositories.Alarm;
+
+const PATHS = require('../../../../router/paths');
+const request = require('../../support/request');
+
+defineSupportCode(({ Given, When, Then }) => {
+  let token;
+  Given(/^\[register\-alarm\] I'm logged as measuring device of unit ID (\d+)$/, (unitId, done) => {
+    unitRepository.findOneById(unitId, (err, unit) => {
+      should.not.exist(err);
+      unit.should.not.be.empty();
+      token = unit.token;
+      done();
+    });
+  });
+  When(/^I register the following reading to the unit ID (\d+) :$/, (unitId, table, done) => {
+    const reading = table.hashes()[0];
+    request.post(PATHS.REGISTER_READING_PATH, reading, token, (error, response, statusCode) => {
+      should.not.exist(error);
+      should.not.exist(response.error);
+      should.exist(response.message);
+      statusCode.should.be.eql(200);
+      done();
+    });
+  });
+
+  Then(/^unit ID (\d+) should have (\d+) alarms$/, (unitId, nAlarms, done) => {
+    alarmRepository.findByUnitId(unitId, (err, alarms) => {
+      alarms.length.should.be.eql(nAlarms);
+      async.each(alarms, (alarm, next) => {
+        alarm.should.have.keys('title', 'description', 'active');
+        next();
+      }, done);
+    });
+  });
+
+  Then(/^unit ID (\d+) shouldn't have any alarm$/, (unitId, done) => {
+    alarmRepository.findByUnitId(unitId, (err, alarms) => {
+      alarms.length.should.be.eql(0);
+      done();
+    });
+  });
+});
