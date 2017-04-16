a73bd65d34f448eaf396c4a4b42ac2f0b9716e43
Iván Portillo Leal <ivanportillo@me.com>
2017-04-16 22:18:17 +0200

    Add limit to findByUnitId method in alarm repository

--- backend/repository/alarmRepository.js
+++ backend/repository/alarmRepository.js
@@ -1,9 +1,10 @@
 'use strict';
 
 module.exports = alarmEntity => {
-  const findByUnitId = (unitId, cb) => {
+  const findByUnitId = (unitId, limit, cb) => {
+    const alarmsLimit = parseInt(limit);
     const query = { unit_id: unitId };
-    alarmEntity.find(query, cb);
+    alarmEntity.find(query, alarmsLimit, cb);
   };
 
   const create = (alarm, cb) => {
