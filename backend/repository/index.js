a5967fb42d9cc2fcc04d86dfaad71cb4928e445c
Iván Portillo Leal <ivanportillo@me.com>
2017-04-16 17:55:44 +0200

    Add alarm repository to repository index

--- backend/repository/index.js
+++ backend/repository/index.js
@@ -7,17 +7,20 @@ const UnitEntity = models.Unit;
 const ReadingEntity = models.Reading;
 const FacilityEntity = models.Facility;
 const OrganizationEntity = models.Organization;
+const AlarmEntity = models.Alarm;
 
 const createUserRepository = require('./userRepository');
 const createFacilityRepository = require('./facilityRepository');
 const createReadingRepository = require('./readingRepository');
 const createUnitRepository = require('./unitRepository');
 const createOrganizationRepository = require('./organizationRepository');
+const createAlarmRepository = require('./alarmRepository');
 
 module.exports = {
   User: createUserRepository(UserEntity),
   Unit: createUnitRepository(UnitEntity),
   Reading: createReadingRepository(ReadingEntity),
   Facility: createFacilityRepository(FacilityEntity),
-  Organization: createOrganizationRepository(OrganizationEntity)
+  Organization: createOrganizationRepository(OrganizationEntity),
+  Alarm: createAlarmRepository(AlarmEntity)
 };
