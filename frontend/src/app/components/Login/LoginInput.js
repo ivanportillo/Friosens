e88eaa5adace1fcc0353ea4d68c9e100840d2c6a
Iván Portillo Leal <ivanportillo@me.com>
2017-04-22 13:03:40 +0200

    Remove style in LoginInput

--- frontend/src/app/components/Login/LoginInput.js
+++ frontend/src/app/components/Login/LoginInput.js
@@ -1,9 +1,8 @@
 import React, { PropTypes } from 'react';
 import Input from 'react-toolbox/lib/input';
-import inputTheme from './input.css';
 
 const LoginInput = ({ type, label, input, meta: { error, touched } }) =>
-  <Input type={type} {...input} label={label} error={touched && error} theme={inputTheme} />;
+  <Input type={type} {...input} label={label} error={touched && error} />;
 
 LoginInput.propTypes = {
   input: PropTypes.object.isRequired,
