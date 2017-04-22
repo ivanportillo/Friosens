b614211e79c88a699cfad4ca7549965279787b6d
Iván Portillo Leal <ivanportillo@me.com>
2017-04-22 13:03:23 +0200

    Add styled components to Login

--- frontend/src/app/components/Login/Login.js
+++ frontend/src/app/components/Login/Login.js
@@ -1,25 +1,76 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import styled from 'styled-components';
+
+import Button from 'react-toolbox/lib/button';
 
 import { Field } from 'redux-form';
 import LoginInput from './LoginInput';
 
-import Button from 'react-toolbox/lib/button';
-import buttonTheme from './button.css';
+const LoggingLabel = styled.label`
+  margin: 1em;
+  padding: 0.5em;
+  color: white;
+  background-color: #8ac148;
+  border-radius: 5px;
+`;
 
-import style from './Login.css';
+const ErrorLabel = styled.label`
+  margin: 1em;
+  padding: 0.5em;
+  color: white;
+  background-color: #ff1744;
+  border-radius: 5px;
+`;
+
+const LoginBox = styled.div`
+  background-color: white;
+  padding: 2em;
+  width: 30em;
+  border-radius: 3px;
+`;
+
+const LoginLayout = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  background-color: #1675d1;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SubmitButton = styled(Button)`
+  &&& {
+    background: #ffa000;
+    color: white;
+  }
+`;
 
 const Login = ({ login, handleSubmit, isLogging, loginError }) =>
-  <div className={style.layout}>
-    <div className={style.loginBox}>
+  <LoginLayout>
+    <LoginBox>
       <h1>FrioSens</h1>
       <form onSubmit={handleSubmit(login)}>
         <Field name="email" type="text" label="E-mail" component={LoginInput} />
         <Field name="password" type="password" label="Contraseña" component={LoginInput} />
-        <Button type="submit" label="Entrar" theme={buttonTheme} raised primary ripple={false} />
-        {isLogging && <label className={style.loading} htmlFor="error">Iniciando sesión...</label>}
-        {loginError && <label className={style.error} htmlFor="error">{loginError}</label> }
+        <SubmitButton type="submit" label="Entrar" ripple={false} />
+        {isLogging && <LoggingLabel htmlFor="error">Iniciando sesión...</LoggingLabel>}
+        {loginError && <ErrorLabel htmlFor="error">{loginError}</ErrorLabel> }
       </form>
-    </div>
-  </div>;
+    </LoginBox>
+  </LoginLayout>;
+
+Login.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  isLogging: PropTypes.bool.isRequired,
+  login: PropTypes.func.isRequired,
+  loginError: PropTypes.string,
+};
+
+Login.defaultProps = {
+  loginError: '',
+};
+
 
 export default Login;
