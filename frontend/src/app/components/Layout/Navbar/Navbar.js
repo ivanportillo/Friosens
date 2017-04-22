9c018a31e38761affa56c25fcb63137c122ed9e7
Iván Portillo Leal <ivanportillo@me.com>
2017-04-22 13:04:33 +0200

    Add styled components to Navbar

--- frontend/src/app/components/Layout/Navbar/Navbar.js
+++ frontend/src/app/components/Layout/Navbar/Navbar.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import styled from 'styled-components';
 
 import AppBar from 'react-toolbox/lib/app_bar';
 import Chip from 'react-toolbox/lib/chip';
 
-import NavbarTheme from './Navbar.css';
+const GreenAppBar = styled(AppBar)`
+  &&& {
+    background-color: #004d40;
+    height: 3.3em;
+    padding-left: 4em;
+    padding-right: 4em;
+  }
+`;
 
 const Navbar = ({ showDrawer }) =>
-  <AppBar
+  <GreenAppBar
     title="FrioSens"
     rightIcon="menu"
     onRightIconClick={showDrawer}
     flat
-    theme={NavbarTheme}
   >
     <Chip>
       <span>Iván Portillo</span>
     </Chip>
-  </AppBar>;
+  </GreenAppBar>;
+
+Navbar.propTypes = {
+  showDrawer: PropTypes.func.isRequired,
+};
 
 export default Navbar;
