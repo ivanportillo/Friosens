b9a6eac55873fb2751d0667e62cb0a5e9cb8db0d
Iván Portillo Leal <ivanportillo@me.com>
2017-04-25 23:00:54 +0200

    Add Content styles to layout

--- frontend/src/app/components/Layout/Layout.js
+++ frontend/src/app/components/Layout/Layout.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Navbar from 'containers/Navbar';
 import DrawerMenu from 'containers/DrawerMenu';
+
+const Content = styled.div` 
+  padding-top: 2.5em;
+  padding-left: 4em;
+  padding-right: 4em;
+  color: #39796b;
+`;
 
 const Layout = ({ children }) =>
   <div>
     <DrawerMenu />
     <Navbar />
-    {children}
+    <Content>
+      {children}
+    </Content>
   </div>;
 
 export default Layout;
