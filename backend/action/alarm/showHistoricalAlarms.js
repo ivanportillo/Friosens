9c0e78034489fbeb2fdbfe9b6346d9f079284043
Iván Portillo Leal <ivanportillo@me.com>
2017-04-25 21:36:19 +0200

    Add limit check to show historical alarms action

--- backend/action/alarm/showHistoricalAlarms.js
+++ backend/action/alarm/showHistoricalAlarms.js
@@ -1,9 +1,20 @@
 'use strict';
 
+const waterfall = require('async').waterfall;
+const ValidationError = require('spur-errors').ValidationError;
+
 module.exports = alarmRepository => {
+  const _checkLimit = (limit, cb) => {
+    if(limit) cb();
+    else cb(ValidationError.create('Limit is required'));
+  };
+
   const _getHistorical = (unitId, limit, cb) => {
     alarmRepository.findByUnitId(unitId, limit, cb);
   };
 
-  return (unitId, limit, callback) => _getHistorical(unitId, limit, callback);
+  return (unitId, limit, callback) => waterfall([
+    next => _checkLimit(limit, next),
+    next => _getHistorical(unitId, limit, next)
+  ], callback);
 };
