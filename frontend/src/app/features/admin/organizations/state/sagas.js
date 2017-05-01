13d5fff8f1363e5dd8c624d54e81caee44427018
Iván Portillo Leal <ivanportillo@me.com>
2017-05-01 14:37:30 +0200

    Create Saga to create organization

--- frontend/src/app/features/admin/organizations/state/sagas.js
+++ frontend/src/app/features/admin/organizations/state/sagas.js
@@ -1,12 +1,16 @@
 import { fork, takeEvery, call, put } from 'redux-saga/effects';
+import { push } from 'react-router-redux';
 
 import * as constants from 'features/admin/organizations/actions/constants';
 import * as organizationsActions from 'features/admin/organizations/actions';
 import * as api from 'infrastructure/api';
 
+import * as PATHS from 'routes/paths';
+
 export function* watchOrganizations() {
   yield fork(watchFetchOrganizations);
   yield fork(watchRemoveOrganization);
+  yield fork(watchCreateOrganization);
 }
 
 export function* fetchOrganizationsProcess() {
@@ -32,10 +36,25 @@ export function* removeOrganizationProcess(action) {
   }
 }
 
+export function* createOrganizationProcess(action) {
+  try {
+    const { data } = yield call(api.createOrganization, action.name, action.orgType);
+    if (data.message) {
+      yield put(push(PATHS.ADMIN_ORGANIZATIONS_PATH.url));
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 export function* watchFetchOrganizations() {
   yield takeEvery(constants.FETCH_ORGANIZATIONS, fetchOrganizationsProcess);
 }
 
 export function* watchRemoveOrganization() {
   yield takeEvery(constants.REMOVE_ORGANIZATION, removeOrganizationProcess);
+}
+
+export function* watchCreateOrganization() {
+  yield takeEvery(constants.CREATE_ORGANIZATION, createOrganizationProcess);
 }
