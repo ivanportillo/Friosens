c61d00ede78c552ed45bec7355a4b6edac705659
Iván Portillo Leal <ivanportillo@me.com>
2017-05-01 11:44:02 +0200

    Adapt Drawer Menu to user/admin

--- frontend/src/app/features/layout/components/Layout/DrawerMenu/DrawerMenu.js
+++ frontend/src/app/features/layout/components/Layout/DrawerMenu/DrawerMenu.js
@@ -3,11 +3,21 @@ import React, { PropTypes } from 'react';
 import Drawer from 'react-toolbox/lib/drawer';
 import { List, ListItem, ListSubHeader, ListDivider } from 'react-toolbox/lib/list';
 
-const DrawerMenu = ({ drawer, hideDrawer, logout }) =>
+const DrawerMenu = ({ drawer, hideDrawer, logout, user }) =>
   <Drawer active={drawer} onOverlayClick={hideDrawer} type="right">
     <List selectable>
       <ListSubHeader caption="Menú" />
-      <ListItem caption="Instalaciones" ripple={false} />
+      {user && !user.admin ?
+        <List selectable>
+          <ListItem caption="Instalaciones" ripple={false} />
+        </List> :
+        <List selectable>
+          <ListItem caption="Organizaciones" ripple={false} />
+          <ListItem caption="Usuarios" ripple={false} />
+          <ListItem caption="Instalaciones" ripple={false} />
+          <ListItem caption="Unidades" ripple={false} />
+        </List>
+      }
       <ListDivider />
       <ListItem caption="Cerrar sesión" ripple={false} onClick={logout} />
     </List>
@@ -17,6 +27,11 @@ DrawerMenu.propTypes = {
   drawer: PropTypes.bool.isRequired,
   hideDrawer: PropTypes.func.isRequired,
   logout: PropTypes.func.isRequired,
+  user: PropTypes.object,
+};
+
+DrawerMenu.defaultProps = {
+  user: null,
 };
 
 export default DrawerMenu;
