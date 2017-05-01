4c8498553d003f39ada43f25531de7744dcbdc98
Iván Portillo Leal <ivanportillo@me.com>
2017-05-01 11:43:15 +0200

    Create actions to organizations

---
+++ frontend/src/app/features/admin/organizations/actions/constants.js
@@ -0,0 +1,3 @@
+export const FETCH_ORGANIZATIONS = 'admin/organizations/FETCH_ORGANIZATIONS';
+export const RECEIVE_ORGANIZATIONS = 'admin/organizations/RECEIVE_ORGANIZATIONS';
+export const REMOVE_ORGANIZATION = 'admin/organizations/REMOVE_ORGANIZATION';
---
+++ frontend/src/app/features/admin/organizations/actions/index.js
@@ -0,0 +1,21 @@
+import * as constants from './constants';
+
+export function fetchOrganizations() {
+  return {
+    type: constants.FETCH_ORGANIZATIONS,
+  };
+}
+
+export function receiveOrganizations(organizations) {
+  return {
+    type: constants.RECEIVE_ORGANIZATIONS,
+    organizations,
+  };
+}
+
+export function removeOrganization(organizationId) {
+  return {
+    type: constants.REMOVE_ORGANIZATION,
+    organizationId,
+  };
+}
