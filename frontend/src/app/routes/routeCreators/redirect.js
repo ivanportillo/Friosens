e00bafe11ce3a5b7ef09fc551e910633057e0a8e
Iván Portillo Leal <ivanportillo@me.com>
2017-05-03 23:04:51 +0200

    Change redirect conditions

--- frontend/src/app/routes/routeCreators/redirect.js
+++ frontend/src/app/routes/routeCreators/redirect.js
@@ -1,23 +1,31 @@
 import React, { PropTypes } from 'react';
 import { Redirect } from 'react-router-dom';
-import { getToken } from 'utils/token';
 
 import * as PATHS from '../paths';
 
-const redirection = ({ needAuth, state }) => {
+const redirection = ({ needAuth, needAdmin, state }) => {
   const currentUser = state.auth.user;
   const isBooting = state.auth.isBooting;
-  if (needAuth && !currentUser && !isBooting) {
-    return <Redirect to={PATHS.LOGIN_PATH.url} />;
+  if (!isBooting) {
+    if ((needAuth || needAdmin) && !currentUser) {
+      return <Redirect to={PATHS.LOGIN_PATH.url} />;
+    }
+    if (currentUser) {
+      if (!needAuth && !currentUser.admin) {
+        return <Redirect to={PATHS.FACILITIES_PATH.url} />;
+      }
+      if (!needAdmin && currentUser.admin) {
+        return <Redirect to={PATHS.ADMIN_ORGANIZATIONS_PATH.url} />;
+      }
+    }
   }
 
-  if (!needAuth && getToken()) {
-    return <Redirect to={PATHS.FACILITIES_PATH.url} />;
-  }
+
   return undefined;
 };
 
 redirection.propTypes = {
+  needAdmin: PropTypes.bool.isRequired,
   needAuth: PropTypes.bool.isRequired,
   state: PropTypes.object.isRequired,
 };
