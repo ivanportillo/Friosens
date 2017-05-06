a267fbbd367ebb2562df69b44a187878b9884bb0
Iván Portillo Leal <ivanportillo@me.com>
2017-05-06 18:15:39 +0200

    Modify FormInput component

--- frontend/src/app/core/components/FormInput/FormInput.js
+++ frontend/src/app/core/components/FormInput/FormInput.js
@@ -1,8 +1,13 @@
 import React, { PropTypes } from 'react';
+import styled from 'styled-components';
 import Input from 'react-toolbox/lib/input';
 
+const StyledInput = styled(Input)`
+  width: ${props => props.width ? props.width : '100%'}
+`;
+
 const FormInput = ({ type, label, input, meta: { error, touched }, ...rest }) =>
-  <Input type={type} {...input} label={label} error={touched && error} {...rest} />;
+  <StyledInput type={type} {...input} label={label} error={touched && error} {...rest} />;
 
 FormInput.propTypes = {
   input: PropTypes.object.isRequired,
