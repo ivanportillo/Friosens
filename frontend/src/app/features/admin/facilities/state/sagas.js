a21a89d3f7031a236a8e2fc612ab71b09f793d46
Iván Portillo Leal <ivanportillo@me.com>
2017-05-06 21:31:20 +0200

    Create sagas to facilities admin

---
+++ frontend/src/app/features/admin/facilities/state/sagas.js
@@ -0,0 +1,63 @@
+import { fork, takeEvery, call, put } from 'redux-saga/effects';
+import { push } from 'react-router-redux';
+
+import * as constants from 'features/admin/facilities/actions/constants';
+import * as facilitiesActions from 'features/admin/facilities/actions';
+import * as api from 'infrastructure/api';
+
+import * as PATHS from 'routes/paths';
+
+export function* watchFacilities() {
+  yield [
+    fork(watchFetchFacilities),
+    fork(watchRemoveFacility),
+    fork(watchCreateFacility),
+  ];
+}
+
+export function* fetchFacilitiesProcess() {
+  try {
+    const { data } = yield call(api.fetchAdminFacilities);
+    if (data) {
+      const facilities = data.data;
+      yield put(facilitiesActions.receiveFacilities(facilities));
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export function* removeFacilityProcess(action) {
+  try {
+    const { data } = yield call(api.removeFacility, action.facilityId);
+    if (data) {
+      yield put(facilitiesActions.fetchFacilities());
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export function* createFacilityProcess(action) {
+  try {
+    const { data } =
+      yield call(api.createFacility, action.name, action.location, action.organization_id);
+    if (data.data) {
+      yield put(push(PATHS.ADMIN_FACILITIES_PATH.url));
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export function* watchFetchFacilities() {
+  yield takeEvery(constants.FETCH_FACILITIES, fetchFacilitiesProcess);
+}
+
+export function* watchRemoveFacility() {
+  yield takeEvery(constants.REMOVE_FACILITY, removeFacilityProcess);
+}
+
+export function* watchCreateFacility() {
+  yield takeEvery(constants.CREATE_FACILITY, createFacilityProcess);
+}
--- frontend/src/app/features/admin/state/sagas.js
+++ frontend/src/app/features/admin/state/sagas.js
@@ -2,10 +2,12 @@ import { fork } from 'redux-saga/effects';
 
 import { watchOrganizations } from 'features/admin/organizations/state/sagas';
 import { watchUsers } from 'features/admin/users/state/sagas';
+import { watchFacilities } from 'features/admin/facilities/state/sagas';
 
 export function* watchAdmin() {
   yield [
     fork(watchOrganizations),
     fork(watchUsers),
+    fork(watchFacilities),
   ];
 }
