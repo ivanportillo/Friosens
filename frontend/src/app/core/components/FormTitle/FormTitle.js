547d5d70e0979914fa097b5ecc56dfb82c3fba49
Iván Portillo Leal <ivanportillo@me.com>
2017-05-06 18:14:13 +0200

    Create FormTitle core component

---
+++ frontend/src/app/core/components/FormTitle/FormTitle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const FormTitle = styled.div`
+  font-size: 1em;
+  font-style: normal;
+  font-weight: normal;
+  padding-bottom: 0.2em;
+  padding-top: 0.5em;
+`;
+
+export default FormTitle;
+
