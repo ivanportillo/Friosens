b50d39d9e0e904eb90ab29c67c6155173d901580
Iván Portillo Leal <ivanportillo@me.com>
2017-05-07 15:04:13 +0200

    Create show units admin feature

---
+++ backend/test/features/facility/show_units_admin.feature
@@ -0,0 +1,27 @@
+@sprint1 @working
+Feature: Show units admin
+  As administrator
+  I want to be able to show the units of the system
+
+  Background:
+    Given [show_units_admin] I'm logged as an administrator
+    And the following organization:
+    | id | name           | type    |
+    | 20 | MyOrganization | company |
+    And the following facilities:
+    | id | name           | location          | organization_id |
+    | 20 | Sala principal | Cubierta exterior |              20 |
+
+  Scenario: Show units
+    Given the following units:
+      | id | name  | location | refrigerant | mark | facility_id |
+      |  1 | unit1 | LOCATION | R410A       | MARK |          20 |
+      |  2 | unit2 | LOCATION | R410A       | MARK |          20 |
+      |  3 | unit3 | LOCATION | R410A       | MARK |          20 |
+    When [show_units_admin] I get units
+    Then [show_units_admin] I should receive 3 units and 200 as status code
+    And each unit should have name, location, refrigerant and token
+
+  Scenario: Show units of a empty facility
+    When [show_units_admin] I get units
+    Then [show_units_admin] I should receive 0 units and 200 as status code
---
+++ backend/test/features/step_definitions/facility/show_units_admin.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const should = require('should');
+const async = require('async');
+const { defineSupportCode } = require('cucumber');
+
+const tokenUt = require('../../../utils/factories/User_factory')();
+
+const PATHS = require('../../../../router/paths');
+const request = require('../../support/request');
+
+defineSupportCode(({ Given, When, Then }) => {
+  let token;
+  let showResponse;
+  Given(/^\[show_units_admin\] I'm logged as an administrator$/, done => {
+    const user = {
+      first_name: 'Nombre',
+      last_name: 'Apellidos',
+      enabled: true,
+      admin: true,
+      email: 'email@dominio.es',
+      password: 'password'
+    };
+    tokenUt.createLogged(user, (err, tokenResult) => {
+      should.not.exist(err);
+      token = tokenResult;
+      done();
+    });
+  });
+
+  When(/^\[show_units_admin\] I get units$/, done => {
+    request.get(PATHS.ADMIN_UNITS_PATH, null, token, (error, response, statusCode) => {
+      should.not.exist(error);
+      showResponse = { response, statusCode };
+      done();
+    });
+  });
+
+  Then(/^\[show_units_admin\] I should receive (\d+) units and (\d+) as status code$/, (nUnits, statusCode, done) => {
+    const units = showResponse.response.data;
+    should(units).have.length(nUnits);
+    should(showResponse.statusCode).be.eql(statusCode);
+    done();
+  });
+
+  Then(/^each unit should have name, location, refrigerant and token$/, done => {
+    async.each(showResponse.response.data, (unit, next) => {
+      should(unit).have.keys('name', 'location', 'refrigerant', 'token');
+      next();
+    }, done);
+  });
+});
