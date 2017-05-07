3ad912a3da55fbc418c63e0e788e1b8b2dca15e8
Iván Portillo Leal <ivanportillo@me.com>
2017-05-07 14:26:28 +0200

    Improve create facility action

--- backend/action/facility/createFacility.js
+++ backend/action/facility/createFacility.js
@@ -6,15 +6,17 @@ const ValidationError = require('spur-errors').ValidationError;
 module.exports = facilityRepository => {
   const _validate = (facility, cb) => {
     if(!facility.name) cb(ValidationError.create('Name is required'));
+    if(!facility.location) cb(ValidationError.create('Location is required'));
+    if(!facility.organization_id) cb(ValidationError.create('Organization is required'));
     else cb();
   };
 
-  const _createFacility = (facility, organizationId, cb) => {
-    facilityRepository.create(facility, organizationId, cb);
+  const _createFacility = (facility, cb) => {
+    facilityRepository.create(facility, facility.organization_id, cb);
   };
 
-  return (facility, userId, callback) => waterfall([
+  return (facility, callback) => waterfall([
     next => _validate(facility, next),
-    next => _createFacility(facility, userId, next)
+    next => _createFacility(facility, next)
   ], callback);
 };
